52b5a9de183af034dcb3911312cb1361eaf053a8
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2016-09-23 18:33:35 +0000

    Release 2.2.2

---
+++ monitor-serial.js
@@ -0,0 +1,19 @@
+var piserialcomm = require('./piserialcomm');
+var adapters = piserialcomm.adapters();
+var processors = piserialcomm.processors();
+var ctx = piserialcomm.context();
+
+var serialAdapter = new adapters.SerialAdpter();
+var serialLogger = new ctx.SerialLogger();
+var cmdProcessor = new processors.LogSerialCommandProcessor();
+
+serialLogger.init(300);
+ctx.SerialContext.init(serialLogger, serialAdapter);
+
+cmdProcessor.debugCommand = function(buf) {
+
+};
+
+serialAdapter.init('/dev/ttyAMA0', cmdProcessor);
+cmdProcessor.init(serialAdapter);
+
